ce14b952684f8edde870beb135edab2c0ee72809
DerekGn <dg89327@hotmail.com>
2021-03-21 18:55:33 +0000

    Added RfmUsb board

---
+++ boards/rfmusb/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+//#define CRYSTALLESS    1
+
+#define VENDOR_NAME "TBD Generic Corp."
+#define PRODUCT_NAME "Rfm Usb V1"
+#define VOLUME_LABEL "RfmUsb"
+#define BOARD_ID "SAMD21E18A-rfmusb-v1"
+
+#define USB_VID 0x04D8
+#define USB_PID 0x2402
+
+//#define LED_PIN PIN_PA23
+//#define LED_TX_PIN PIN_PA27
+//#define LED_RX_PIN PIN_PB03
+
+#endif
