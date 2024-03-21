e37db88aee0ce18c5d6b69ad3befe9a1f138b115
Derek Goslin <dg89327@hotmail.com>
2024-03-21 18:28:46 +0000

    Added RfmEm

---
+++ boards/rfmEm/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+//#define CRYSTALLESS    1
+
+#define VENDOR_NAME "AxioLogix Technologies"
+#define PRODUCT_NAME "Rfm-Em"
+#define VOLUME_LABEL "RfmEm"
+#define BOARD_ID "SAMD21E18A-rfmem"
+
+#define USB_VID 0xDEAD
+#define USB_PID 0xFEED
+
+//#define LED_PIN PIN_PA23
+//#define LED_TX_PIN PIN_PA27
+//#define LED_RX_PIN PIN_PB03
+
+#endif
